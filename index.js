97d428cdb1e954cf7519eeba030505e0d6c1c7a2
Juan Pablo Gonzales <juampi7237@gmail.com>
2022-10-07 14:49:00 -0400

    creando mas rutas y respondiendo jsons

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) => {
   res.send('Hola mi primer server en express');
 });
 
+app.get('/ruta', (req, res) => {
+  res.send('Hola desde la nueva ruta');
+});
+
+app.get('/productos', (req, res) => {
+  res.json({
+    name: 'producto 1',
+    price: 10000,
+  });
+});
+
 app.listen(port, () => {
   console.log(`Mi port ${port}`);
 });
