1fa1b069b3c0ec36fa3b492e077befbab78bdfc7
Juan Pablo Gonzales <juampi7237@gmail.com>
2022-10-07 20:24:12 -0400

    creando rutas mas complejas y usando res.params para acceder a los parametro de la URL

--- index.js
+++ index.js
@@ -11,9 +11,34 @@ app.get('/ruta', (req, res) => {
 });
 
 app.get('/productos', (req, res) => {
+  res.json([
+    {
+      name: 'producto 1',
+      price: 10000,
+    },
+    {
+      name: 'producto 2',
+      price: 5000,
+    },
+  ]);
+});
+
+app.get('/productos/:id', (req, res) => {
+  // const id = req.params.id;
+  const { id } = req.params;
   res.json({
-    name: 'producto 1',
-    price: 10000,
+    id,
+    name: 'producto',
+    price: 800,
+  });
+});
+
+app.get('/categoria/:categoriaId/productos/:productoId', (req, res) => {
+  // const id = req.params.id;
+  const { categoriaId, productoId } = req.params;
+  res.json({
+    categoriaId,
+    productoId,
   });
 });
 
